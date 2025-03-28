055018548e59f7f5b0aae68f14eca3f9d266e82f
LelinZheng <lezi.lelin@gmail.com>
2025-03-28 10:16:45 -0700

    Added Error Handling and Input Validation both in Client-Side Form and the  Back End using Joi

--- Middleware_into/index.js
+++ Middleware_into/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const AppError = require('./AppError');
 
 // app.use( () => {
 //     console.log('HEYYY');
@@ -33,7 +34,7 @@ const verifyPassword = ((req, res, next) => { // if we do app.use, we have to pu
     if(password === 'cocacola'){
         next();
     }
-    res.send("SORRY YOU NEED PASSWORD!");
+    throw new AppError('Password required!', 401);
     // http://localhost:3000/?password=cocacola need to write password as such
 })
 
@@ -49,6 +50,9 @@ app.get('/', (req, res) => {
     res.send('HOME PAGE!');
 })
 
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an admin', 403);
+})
 app.get('/dogs', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('WOOF WOOF!');
@@ -58,14 +62,25 @@ app.get('/secrets', verifyPassword, (req, res) => { // it will run verifyPasswor
     res.send('THIS IS A TOP SECRET!');
 })
 
+app.get('/error', (req, res) => { 
+    chicken; // this will trigger default error message to be 'chicken is not defined'
+})
+
 app.use((req, res) => { // to catch all other ones
     res.status(404).send("NOT FOUND!")
 })
 
-app.use((req, res) => { // to catch all other ones
-    res.send('404 - NOT FOUND');
-})
 
+// app.use((err, req, res, next) => { // catch errors
+//     console.log("****************************");
+//     res.status(500).send("OH BOYYY, THERE IS AN ERROR!");
+//     console.log(err)
+//     next(err); // we need to pass in the error to pass into the next error handling
+// })
+app.use((err, req, res, next) => {
+    const { status=500, message = 'Something Went Wrong' } = err; // default to 500 so any other error (not AppError) would be catched and handled
+    res.status(status).send(message); 
+})
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
 })
